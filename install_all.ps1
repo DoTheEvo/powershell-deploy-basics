33e825372873302a68d67cc065d05152932ee199
DoTheEvolution <DoTheEvo@gmail.com>
2016-08-02 22:01:43 +0200

    fix for powershell v2.0

--- install_all.ps1
+++ install_all.ps1
@@ -1,7 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
-$programs_dirs = Get-ChildItem $parent_dir -dir | Select-Object FullName
+$programs_dirs = Get-ChildItem $parent_dir | ?{ $_.PSIsContainer } | Select-Object FullName
 
 $programs_dirs | ForEach-Object {
     $sub_path = $_.FullName
@@ -9,7 +9,14 @@ $programs_dirs | ForEach-Object {
     & "$sub_path\$script_name.ps1"
 }
 
+$win_vers = [Environment]::OSVersion.Version.Major
+if ($win_vers -ge 10) {
+    echo "Win10 - removing preinstalled apps"
+    & "$parent_dir\win10.ps1"
+}
+
 echo "ALL DONE"
 echo "ALL DONE"
 echo "ALL DONE"
+echo "Panda - turn off panda news, turn off usb scan recommendation"
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
