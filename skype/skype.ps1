afa001cc26edd89e42fde41e273639adc1bfd42c
DoTheEvolution <DoTheEvo@gmail.com>
2016-08-03 00:32:43 +0200

    skype no desktop icon, readme update

--- skype/skype.ps1
+++ skype/skype.ps1
@@ -5,5 +5,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
 Start-Process -FilePath "$parent_dir\SkypeSetup.msi" -ArgumentList '/qn','ALLUSERS=1','TRANSFORMS=:RemoveStartup.mst' -Wait
 
+Remove-Item "C:\Users\Public\Desktop\Skype.lnk" -ErrorAction SilentlyContinue
+
 echo "SKYPE DONE"
 echo "------------------------------------------------- "
