461715058c76e4e510b2086240e55b42ce2745dc
DoTheEvolution <DoTheEvo@gmail.com>
2016-08-03 21:27:15 +0200

    update firefox, chrome, libreoffice

--- chrome/chrome.ps1
+++ chrome/chrome.ps1
@@ -3,7 +3,7 @@ echo "INSTALLING CHROME"
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
-Start-Process -FilePath "$parent_dir/ChromeStandaloneSetup.exe" -ArgumentList '/silent','/install' -Wait
+Start-Process -FilePath "$parent_dir/ChromeStandaloneSetup64.exe" -ArgumentList '/silent','/install' -Wait
 
 echo "CHROME DONE"
 echo "------------------------------------------------- "
--- libreoffice/libreoffice.ps1
+++ libreoffice/libreoffice.ps1
@@ -3,8 +3,8 @@ echo "INSTALLING LIBREOFFICE"
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
-Start-Process -FilePath "$parent_dir\LibreOffice_5.1.1_Win_x86.msi" -ArgumentList '/quiet','/qn','ADDLOCAL=ALL','CREATEDESKTOPLINK=1','REGISTER_ALL_SMO_TYPES=1','REMOVE=gm_o_Onlineupdate','RebootYesNo=No' -Wait
-Start-Process -FilePath "$parent_dir\LibreOffice_5.1.1_Win_x86_helppack_sk.msi" -ArgumentList '/quiet','/qn' -Wait
+Start-Process -FilePath "$parent_dir\LibreOffice_5.2.0_Win_x64.msi" -ArgumentList '/quiet','/qn','ADDLOCAL=ALL','CREATEDESKTOPLINK=1','REGISTER_ALL_SMO_TYPES=1','REMOVE=gm_o_Onlineupdate','RebootYesNo=No' -Wait
+Start-Process -FilePath "$parent_dir\LibreOffice_5.2.0_Win_x64_helppack_sk.msi" -ArgumentList '/quiet','/qn' -Wait
 
 echo "LIBREOFFICE DONE"
 echo "------------------------------------------------- "
