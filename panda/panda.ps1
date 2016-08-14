67d018a5d795e01c997f9716f7188a57197740ff
DoTheEvolution <DoTheEvo@gmail.com>
2016-08-14 17:01:55 +0200

    change unzip function

--- panda/panda.ps1
+++ panda/panda.ps1
@@ -1,37 +1,23 @@
-function Expand-ZIPFile($file, $destination){
-    $shell = new-object -com shell.application
-    $zip = $shell.NameSpace($file)
-    foreach($item in $zip.items()){
-        $shell.Namespace($destination).copyhere($item)
-    }
-}
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
 echo "INSTALLING PANDA"
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $parent_dir = Split-Path $MyInvocation.MyCommand.Path
 
+echo "EXTRACTING THE ZIP FILE"
+
 if ($PSVersionTable.PSVersion.Major -ge 5) {
     Expand-Archive "$parent_dir\panda.zip" "$parent_dir"
 
-} ElseIf ($PSVersionTable.PSVersion.Major -ge 4) {
-    echo "win 8.1 powershell"
-    Unzip "$parent_dir\panda.zip" "$parent_dir"
-} Else {
-    echo "win 7 powershell"
-    Expand-ZIPFile –File "$parent_dir\panda.zip" –Destination "$parent_dir"
+} else {
+    echo "older powershell"
+    $shellApplication = new-object -com shell.application
+    $zipPackage = $shellApplication.NameSpace("$parent_dir\panda.zip")
+    $destinationFolder = $shellApplication.NameSpace($parent_dir)
+    $destinationFolder.CopyHere($zipPackage.Items(), 20)
 }
 
-echo "zip extracted"
+echo "ZIP FILE EXTRACTED"
 
 Start-Sleep -m 2000
 
@@ -39,6 +25,7 @@ Start-Process -FilePath "$parent_dir\Panda Free Antivirus 16.1.3\setup.exe" -Arg
 Remove-Item "C:\Users\Public\Desktop\Panda Free Antivirus.lnk" -ErrorAction SilentlyContinue
 
 Remove-Item -Recurse -Force "$parent_dir\Panda Free Antivirus 16.1.3" -ErrorAction SilentlyContinue
+echo "EXTRACTED FOLDER DELETED"
 
 echo "PANDA DONE"
 echo "------------------------------------------------- "
