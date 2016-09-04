6bad3934c05e515b81f66d0e7320f7e5aac767a7
DoTheEvolution <DoTheEvo@gmail.com>
2016-09-04 18:08:10 +0200

    cleaner, updates, panda off for now, chrome ublock on install, needs internet connection though

--- panda/panda.ps1
+++ panda/panda.ps1
@@ -4,19 +4,19 @@ $parent_dir = Split-Path $MyInvocation.MyCommand.Path
 
 echo "- extracting the zip file"
 
-if ($PSVersionTable.PSVersion.Major -ge 5) {
-    Expand-Archive "$parent_dir\panda.zip" "$parent_dir"
-} else {
-    $shellApplication = new-object -com shell.application
-    $zipPackage = $shellApplication.NameSpace("$parent_dir\panda.zip")
-    $destinationFolder = $shellApplication.NameSpace($parent_dir)
-    $destinationFolder.CopyHere($zipPackage.Items(), 20)
-}
+# if ($PSVersionTable.PSVersion.Major -ge 5) {
+#     Expand-Archive "$parent_dir\panda.zip" "$parent_dir"
+# } else {
+#     $shellApplication = new-object -com shell.application
+#     $zipPackage = $shellApplication.NameSpace("$parent_dir\panda.zip")
+#     $destinationFolder = $shellApplication.NameSpace($parent_dir)
+#     $destinationFolder.CopyHere($zipPackage.Items(), 20)
+# }
 
-Start-Sleep -m 1000
+# Start-Sleep -s 1
 
-Start-Process -FilePath "$parent_dir\panda\setup.exe" -ArgumentList '/quiet' -Wait
-Remove-Item "C:\Users\Public\Desktop\Panda Free Antivirus.lnk" -ErrorAction SilentlyContinue
+# Start-Process -FilePath "$parent_dir\panda\setup.exe" -ArgumentList '/quiet' -Wait
+# Remove-Item "C:\Users\Public\Desktop\Panda Free Antivirus.lnk" -ErrorAction SilentlyContinue
 
 # Remove-Item -Recurse -Force "$parent_dir\Panda Free Antivirus 17.0.1" -ErrorAction SilentlyContinue
 
