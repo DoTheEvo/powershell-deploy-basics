eec696290ab82994723100992094a0a55e5a8cc2
DoTheEvolution <DoTheEvo@gmail.com>
2016-09-20 22:17:58 +0200

    comment

--- 0win7_updates/0win7_updates.ps1
+++ 0win7_updates/0win7_updates.ps1
@@ -3,6 +3,7 @@ $minor_vers = [Environment]::OSVersion.Version.Minor
 if ($major_vers -eq 6) {
     if ($minor_vers -eq 1) {
         echo "WINDOWS 7 DETECTED - INSTALLING 3 UPDATES TO PREVENT LONG WAIT ON FIRST UPDATE"
+        # KB3050265 KB3083710 KB3102810
 
         $parent_dir = Split-Path $MyInvocation.MyCommand.Path
          Foreach($item in (ls $parent_dir *.msu -Name))
